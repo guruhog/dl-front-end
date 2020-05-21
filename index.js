026366d18451d54acf157ee3698b7b9a94c8f743
Ian Hamriding <guruhog@gmail.com>
2020-05-21 12:45:31 +0200

    5

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+// const https = require('https');
+// const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ limit: '1000mb' }));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(_, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const PORT = '8080';
+
+// const key = fs.readFileSync('ssl/key.pem', 'utf8');
+// const cert = fs.readFileSync('ssl/cert.pem', 'utf8');
+
+// const credentials = { key, cert };
+// const server = https.createServer(credentials, app);
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
