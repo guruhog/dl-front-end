a77291981f9fbb8fcb2a48d4ba5eafc536dad979
Ian Hamriding <guruhog@gmail.com>
2020-07-05 19:59:56 +0200

    updated express routing2

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ app.use(bodyParser.json({ limit: "1000mb" }));
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"), options, function (
-    err
-  ) {
+  res.sendFile(path.join(__dirname, "build", "index.html"), function (err) {
     if (err) {
       res.status(500).send(err);
     }
