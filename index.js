d7044f78d43fe4cb56dfc018509ef3c3bf5bcda3
Ian Hamriding <guruhog@gmail.com>
2020-07-05 19:52:26 +0200

    updated express routing

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 // const https = require('https');
 // const fs = require('fs');
-const path = require('path');
+const path = require("path");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({ limit: '1000mb' }));
+app.use(bodyParser.json({ limit: "1000mb" }));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  if (err) {
-    res.status(500).send(err)
-  }
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"), options, function (
+    err
+  ) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
-const PORT = '8080';
+const PORT = "8080";
 
 // const key = fs.readFileSync('ssl/key.pem', 'utf8');
 // const cert = fs.readFileSync('ssl/cert.pem', 'utf8');
